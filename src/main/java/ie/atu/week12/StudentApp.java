21ab3d60623f8ee6da62f82a80692fa84bcbf0c7
g00399850 <g00399850@atu.ie>
2022-12-12 10:37:55 +0000

    Created a class called "StudentApp"

--- src/main/java/ie/atu/week12/StudentApp.java
+++ src/main/java/ie/atu/week12/StudentApp.java
@@ -1,4 +1,15 @@
 package ie.atu.week12;
 
 public class StudentApp {
+    private String name1;
+    private double num1;
+    private double num2;
+    private String name2;
+
+    public StudentApp(String name1, double num1, String name2, double num2){
+        this.name1 = name1;
+        this.num1 = num1;
+        this.num2 = num2;
+        this.name2 = name2;
+    }
 }
